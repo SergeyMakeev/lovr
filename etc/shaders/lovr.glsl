9446fcea3e3888993e0a4882e41b3442f3e8d834
bjorn <mail@bjornbyt.es>
2025-07-24 08:25:08 -0700

    webgpu: point size is not supported;

--- etc/shaders/lovr.glsl
+++ etc/shaders/lovr.glsl
@@ -534,7 +534,7 @@ void main() {
     Tangent = vec4(NormalMatrix * VertexTangent.xyz, VertexTangent.w);
   }
 
-  PointSize = flag_pointSize;
+  PointSize = 1.0;
   Position = lovrmain();
 
   if (flag_uvTransform) {
