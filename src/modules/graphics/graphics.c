4cfab98a155f032943375d08970854c7e9e0041a
bjorn <mail@bjornbyt.es>
2025-08-02 19:38:26 -0700

    webgpu: flip y;

--- src/modules/graphics/graphics.c
+++ src/modules/graphics/graphics.c
@@ -354,6 +354,7 @@ struct Readback {
 typedef struct {
   float resolution[2];
   float time;
+  float clipSignY;
 } Globals;
 
 typedef struct {
@@ -1397,6 +1398,11 @@ static bool recordRenderPass(Pass* pass, gpu_stream* stream) {
   global->time = lovrTimerGetTime();
 #else
   global->time = lovrHeadsetIsActive() ? lovrHeadsetGetDisplayTime() : lovrTimerGetTime();
+#endif
+#ifdef LOVR_WEBGPU
+  global->clipSignY = -1.f;
+#else
+  global->clipSignY = 1.f;
 #endif
 
   // Cameras
@@ -8575,6 +8581,9 @@ bool lovrPassText(Pass* pass, ColoredString* strings, uint32_t count, float* tra
 
   Material* material;
   bool flip = pass->cameraCount > 0 && pass->cameras[(pass->cameraCount - 1) * pass->views].projection[5] > 0.f;
+#ifdef LOVR_WEBGPU
+  flip = !flip;
+#endif
   if (!lovrFontGetVertices(font, strings, count, wrap, halign, valign, vertices, &glyphCount, &lineCount, &material, flip)) {
     stackPop(&thread.stack, stack);
     return false;
